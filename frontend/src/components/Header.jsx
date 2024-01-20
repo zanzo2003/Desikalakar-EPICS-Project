03225b88b2cdd530f212021d48bf4fb6c112654e
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2024-01-20 13:12:20 +0530

    Home and shoping navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser, FaHome } from 'react-icons/fa';
+import { FaShoppingBag } from "react-icons/fa";
 
 import React from 'react'
 
@@ -8,12 +9,13 @@ function Header() {
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
+                <Navbar.Brand href="/home">
                     <FaHome/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                <Navbar.Collapse id='basic-navbar-nav'>
                    <Nav className='ms-auto'>
+                   <Nav.Link href='/'><FaShoppingBag/>Shop</Nav.Link>
                     <Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
                     <Nav.Link href='/login'><FaUser/>Sign In</Nav.Link>
                    </Nav>
