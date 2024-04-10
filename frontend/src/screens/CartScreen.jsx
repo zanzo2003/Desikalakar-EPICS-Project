47f5c252463c7db456a82e5b35d31a227fe4cb12
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2024-04-10 21:15:18 +0530

    cart design

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -81,7 +81,7 @@ const CartScreen = () => {
                         <h3>
                             Subtotal ({cartItems.reduce((acc, item) => acc+item.qty, 0)}) Items
                         </h3>
-                        Rs{cartItems.reduce((acc, item)=> acc+item.qty * item.price, 0).toFixed
+                        Rs {cartItems.reduce((acc, item)=> acc+item.qty * item.price, 0).toFixed
                         (2)}
                     </ListGroup.Item>
                 </ListGroup>
